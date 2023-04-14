5a1f117587c53c2a714ea05c573e5271b3d463c5
Sakura <Sakura__L@outlook.com>
2023-04-15 01:22:05 +0800

    pic2str

--- FIG2PDF.py
+++ FIG2PDF.py
@@ -5,28 +5,32 @@
 # Description:fig to pdf
 
 import glob
+import os
+
 
 # TODO: 整合进gui界面，要求：识别图片格式（jpg，png等，输入路径即可转换为PDF
-
-
-
-# doc = fitz.open()
-
 
 def pic2pdf(source_folder):
     doc = fitz.open()
     try:
+        source_folder = source_folder.replace('\\', '/')
+        source_folder = source_folder + "" if source_folder.endswith("/") else source_folder + "/"
         print(source_folder)
-        source_folder = source_folder + "*" if source_folder.endswith("\\") else source_folder + "\\*"
-        print(source_folder)
-        for img in sorted(glob.glob(source_folder),
-                          key=lambda x: int(str(x).split("4233_")[1].split('-gig')[0])):  # 读取图片，确保按文件名排序
+        included_extensions = ['jpg', 'jpeg', 'png']
+        included_extensions += [i.upper() for i in included_extensions]
+        file_names = [source_folder+fn for fn in os.listdir(source_folder)
+                      if any(fn.endswith(ext) for ext in included_extensions)]
+
+        print(file_names)
+        # for img in sorted(glob.glob(source_folder),
+        #                   key=lambda x: int(str(x).split("4233_")[1].split('-gig')[0])):  # 读取图片，确保按文件名排序
+        for img in file_names:
             print('img:', img)
             imgdoc = fitz.open(img)  # 打开图片
             pdfbytes = imgdoc.convert_to_pdf()  # 使用图片创建单页的 PDF
             imgpdf = fitz.open("pdf", pdfbytes)
             doc.insert_pdf(imgpdf)  # 将当前页插入文档
-        doc.save("target.pdf")  # 保存pdf文件
+        doc.save(source_folder+"target.pdf")  # 保存pdf文件
     except Exception as e:
         print(e)
         print("目录：[ %s ] 转换pdf异常" % source_folder)
@@ -37,5 +41,5 @@ def pic2pdf(source_folder):
 if __name__ == '__main__':
     import fitz
 
-    img_path = input('输入地址：')
+    img_path = input('输入图片存放地址：')
     pic2pdf(img_path)
